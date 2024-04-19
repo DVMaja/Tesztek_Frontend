9b1f2c2e1000d21fb37983f43301372e7bbf4410
DVMaja <majadreilinger@gmail.com>
2024-04-19 13:22:10 +0200

    üres alap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">DOGA</header>
+      <article>
+        
+      </article>
+    </div>
+  );
+}
+
+export default App;
